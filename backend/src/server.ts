55c84fcccfc3a65a79bdc7fbce68f065251a533d
EmersonGomes21 <emerson.cursos.ti@gmail.com>
2020-08-05 04:16:36 -0300

    adding backend starting

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+  const users = [
+    { name: 'Diego', age: 25 },
+    { name: 'Vini', age: 21 }
+  ]
+  return response.json(users);
+
+})
+
+
+app.listen(3333, () => {
+  console.log('API ONLINE');
+});
