5c3d2b82671debe5d83a10c406b1daa15aeaac3b
EmersonGomes21 <emerson.cursos.ti@gmail.com>
2020-08-05 02:15:27 -0300

    updating Page Landing and styles

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import {Link} from 'react-router-dom'
 import logo from '../../assets/images/logo.svg';
 import landing from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
@@ -9,7 +11,6 @@ import './styles.css';
 function Landing() {
   return (
     <div id="page-landing">
-
       <div id="page-landing-content" className="container">
 
         <div className="logo-container">
@@ -22,19 +23,21 @@ function Landing() {
           src={landing}
           alt="Plataforma de estudos"
           className="hero-image" />
-        <div className="buttons-container">
           
-          <a href="" className="study" >
+        <div className="buttons-container">
+
+          <Link to="/study" className="study" >
            <img src={studyIcon} alt="Estudar" />
            Estudar
-          </a>
+          </Link>
 
-          <a href="" className="give-classes">
-           <img src={giveClassesIcon} alt="Estudar" />
-           Estudar
-          </a>
+          <Link to="/give-classes" className="give-classes">
+           <img src={giveClassesIcon} alt="Dar aulas" />
+           Dar aulas
+          </Link>
 
         </div>
+
         <span className="total-connections">
           Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
         </span>
